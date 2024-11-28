9d11b1b30e8825e8eead798f4e20d90096d223f3
Khushal Dayala <khushal.dayala@bytestechnolab.com>
2024-11-28 14:48:49 +0530

    first commit

--- src/Config.php
+++ src/Config.php
@@ -16,8 +16,8 @@ class Config
     {
         if ($filePath && file_exists($filePath)) {
             $this->config = require $filePath;
-        } elseif (file_exists(__DIR__ . '/../config/buyerlistapi.php')) {
-            $this->config = require __DIR__ . '/../config/buyerlistapi.php';
+        } elseif (file_exists(__DIR__ . '/../../../../config/buyerlistapi.php')) {
+            $this->config = require __DIR__ . '/../../../../config/buyerlistapi.php';
         } else {
             throw new \Exception('Configuration file not found.');
         }
