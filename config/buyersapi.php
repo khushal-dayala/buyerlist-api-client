f188922948e539f5d92fd2be67b12160b5d0b59a
Khushal Dayala <khushal.dayala@bytestechnolab.com>
2024-12-02 11:46:49 +0530

    first commit

--- config/buyersapi.php
+++ config/buyersapi.php
@@ -5,7 +5,12 @@ return [
     'default_params' => [
         'ckm_campaign_id' => '2590',
         'ckm_key'         => 'FTuoNmuSGs',
+        'ckm_offer_id'    => '123',
+        'state'           => 'CA',
+        'zipcode'         => '12345',
+        'email_address'   => 'ckmtest@gmail.com',
+        'best_time'       => 'Afternoon',
         'country'         => 'US',
-        'opt_in'          => 'true',
+        'opt_in'          => 'true'
     ],
 ];
--- src/Config.php
+++ src/Config.php
@@ -7,27 +7,24 @@ class Config
     protected $config = [];
 
     /**
-     * Constructor to load or create the configuration file.
+     * Constructor to load the configuration file.
      *
      * @param string|null $filePath Path to the custom configuration file
      * @throws \Exception
      */
     public function __construct($filePath = null)
     {
-        $rootConfigFolder = __DIR__ . '/config';
-        $defaultConfigFile = $rootConfigFolder . '/buyerlistapi.php';
+        $defaultConfigFile = __DIR__ . '/config/buyerlistapi.php';
 
         // Use custom file if provided
         if ($filePath && file_exists($filePath)) {
             $this->config = require $filePath;
         }
-        // Check if default config file exists
+        // Use default config file if no custom file is provided
         elseif (file_exists($defaultConfigFile)) {
             $this->config = require $defaultConfigFile;
         } else {
-            // Create the config folder and file if they don't exist
-            $this->createDefaultConfig($rootConfigFolder, $defaultConfigFile);
-            $this->config = require $defaultConfigFile; // Load the newly created config
+            throw new \Exception('Configuration file not found.');
         }
     }
 
@@ -50,41 +47,5 @@ class Config
     public function all()
     {
         return $this->config;
-    }
-
-    /**
-     * Create the default configuration folder and file.
-     *
-     * @param string $configFolder Path to the configuration folder
-     * @param string $configFile Path to the configuration file
-     * @throws \Exception
-     */
-    private function createDefaultConfig($configFolder, $configFile)
-    {
-        // Create the config folder if it doesn't exist
-        if (!is_dir($configFolder)) {
-            if (!mkdir($configFolder, 0755, true) && !is_dir($configFolder)) {
-                throw new \Exception('Failed to create configuration folder.');
-            }
-        }
-
-        // Create the default config file
-        $defaultConfigContent = <<<'PHP'
-<?php
-
-return [
-    'base_url' => 'https://exttrk.cakemarketing.com/d.ashx',
-    'default_params' => [
-        'ckm_campaign_id' => '2590',
-        'ckm_key'         => 'FTuoNmuSGs',
-        'country'         => 'US',
-        'opt_in'          => 'true',
-    ],
-];
-PHP;
-
-        if (!file_put_contents($configFile, $defaultConfigContent)) {
-            throw new \Exception('Failed to create configuration file.');
-        }
     }
 }
